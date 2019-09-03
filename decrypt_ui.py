ec89c1bebe1f63b65b8bb21cbc24f6b15e476949
suptoasty <jason.lonsinger@gmail.com>
2019-09-03 12:02:57 -0500

    Initial Commit

---
+++ decrypt_ui.py
@@ -0,0 +1,119 @@
+from decryptorator5000 import read_text, print_list, is_solved, solve
+import random
+import sys
+import types
+
+import PyQt5
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QWidget, QLabel, QApplication, QVBoxLayout, QHBoxLayout, QLayout, QPushButton, QMainWindow, QFileDialog, QTextEdit
+
+
+# !!! Im using an old pyqt5 file i used to learn qt5 !!!
+#####
+class app(QApplication):
+    def __init__(self, args):
+        QApplication.__init__(self, args)
+        print("Arguments-> ", args)
+
+
+class window(QWidget):
+    __layout = QVBoxLayout()
+
+    def __init__(self, widget: QWidget = None):
+        QWidget.__init__(self)
+
+        if(not widget is None):
+            self.__layout.addWidget(widget)
+        self.setLayout(self.__layout)
+
+    def getLayout():
+        return __layout
+
+
+class wid(QWidget):
+    __widgets = []
+    __layout = QVBoxLayout()
+
+    def __init__(self, widgets: [] = None, layout: QLayout = None):
+        QWidget.__init__(self)
+        if(not widgets is None):
+            self.__widgets = widgets
+        if(not layout is None):
+            self.__layout = layout
+
+        for i in self.__widgets:
+            self.__layout.addWidget(i)
+            print(i)
+        self.setLayout(layout)
+
+    def getLayout():
+        return __layout
+
+    # def _setLayout(layout: QLayout):
+    #     self.__layout = layout
+    #     QWidget.setLayout(layout)
+
+    def getWidgets():
+        return __widgets
+
+    def pushWidget(widget: QWidget):
+        __widgets.append(widget)
+#####
+
+
+
+# Driver Below
+cipher: list = None
+plaintext: list = None
+def open_file(dia: QFileDialog, cipher_text: QTextEdit, plaintext_text: QTextEdit):
+	file = dia.getOpenFileName()
+	cipher = read_text(file[0])
+	cipher_text.setText(str(cipher))
+
+	plaintext = solve(list(cipher))
+	plaintext_text.setText(str(plaintext))
+
+def solve_cipher(cipher_text: QTextEdit, plaintext_text: QTextEdit):
+    if cipher_text.toPlainText() is "":
+        print("No Text in Cipher object")
+        return
+    # if cipher is None:
+    #     print("No Cipher loaded")
+    #     return -1
+    plaintext = solve(list(cipher_text.toPlainText()))
+    plaintext_text.setText(str(plaintext))
+    return plaintext
+
+
+if __name__ == "__main__":
+	# get commandline args and create new app
+	m_app = app(sys.argv)
+	
+	# make new label and button for file selection
+	m_button = QPushButton("Choose File")
+	m_label = QLabel("Select File: ")
+	
+	m_cipher_text = QTextEdit("")
+	# m_cipher_text.setDisabled(True)
+	m_cipher_label = QLabel("Cipher: ")
+	
+	m_plain_text = QTextEdit("")
+	m_plain_text.setDisabled(True)
+	m_plaintext_label = QLabel("Plain Text: ")
+	
+	m_solve_btn = QPushButton("Solve")
+	m_solve_btn.clicked.connect(lambda: solve_cipher(m_cipher_text, m_plain_text))
+
+	# new layout widget
+	m_widget = wid([m_label, m_button, m_cipher_label, m_cipher_text, m_plaintext_label, m_plain_text, m_solve_btn], QVBoxLayout())
+
+	# new file chooser
+	m_file_chooser = QFileDialog(m_widget, "Choose File", ".", "")
+	m_button.clicked.connect(lambda: open_file(m_file_chooser, m_cipher_text, m_plain_text))
+	
+	# new window
+	m_window = window(m_widget)
+	m_window.show()
+	
+	# main loop till exit
+	sys.exit(m_app.exec_())
---
+++ decryptorator5000.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+import io
+import re
+import os
+import types
+import time
+import binascii
+# import importlib
+
+# moduleName = input('Decrypt')
+# importlib.import_module('Decrypt')
+
+previous_shifts: list = [0] #store the previous shifts tested when decrypting
+
+def encrypt(cipher:list, shift_amount: int)-> str:
+	asc: list = []
+	word: str
+	for word in cipher:
+		# asc.append(word.encode("ascii", "replace"))
+		character: chr
+		for character in word:
+			# print(character.encode("ascii", "replace"))
+			print(binascii.a2b_base64(word))
+	return asc
+
+def is_solved(cipher: list, plaintext: list)-> bool:
+	word: str
+	for word in cipher:
+		#if word is the same then cryptogram is not solved
+		if(word in plaintext):
+			return False
+		#if characters at the same position are in both lists then not solved
+		character: chr
+		for character in word:
+			return False
+	return True
+
+"""
+Solve using 
+"""
+def solve(cipher: list)-> list:
+	plaintext: list = []
+	while not is_solved(cipher, plaintext):
+		break
+	plaintext.append("need")
+	plaintext.append("to")
+	plaintext.append("actual")
+	plaintext.append("solve")
+	return plaintext
+
+# lambda/regex function for converting cipher to map of words then a more useful list after stripping undesired symbols
+def text_to_list(text: str)-> list:
+    return list(map(lambda x: re.sub("[,.!?]", "", x).lower(), text.split()))
+    """
+    *Sorry for the bad form of multiline string as a comment and for the long comment*
+    I think this runs in O(x)? My lambda calculus skills are non existant and my Big O are minimal
+    You're probably millennia ahead of what my Algorithm Analysis skills could ever be Prof. Nix
+    If you could take the time to tell me what this actually runs at, that would be appreciated
+    """
+
+# read text from file
+def read_text(file_name: str)-> list:
+	file = open(file_name, 'r')
+	text: str = file.read()
+	file.close()
+	return text_to_list(text) # doest this prevent garbage collection on text??
+
+# read from console
+def read_console()-> list:
+    return text_to_list(input("Input Text: "))
+
+
+# prints words without newline
+def print_list(cipher: list):
+	word: str
+	for word in cipher:
+		print(word, end=" ")
+
+
+if __name__ == "__main__":
+	time_before: int = int(round(time.time() * 1000))
+	use_file: bool = None
+	while True:
+		text: str = input("Would you like to use a file (y/n): ")
+		if(text.lower() in ['y', 'yes', 'true']):
+			use_file = True
+			break
+		elif(text.lower() in ['n', 'no', 'false']):
+			use_file = False
+			break
+		else:
+			print("Please Try again using Yes or No: ")
+	cipher: list = None
+	if(use_file):
+		while True:
+			file: str = input("File Path (Local or Global): ")
+			if(os.path.exists(file)):
+				cipher = read_text(file)
+				break
+			else:
+				print("Invlaid File Path Try Again: ")
+	else:
+		cipher = read_console()
+	plaintext: list = solve(cipher)
+	print_list(cipher)
+	print("MSecs: ", int(round(time.time() * 1000))-time_before)
